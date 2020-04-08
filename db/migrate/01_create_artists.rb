e1ac413c0bb47a587cafe973e2a6dc8d2635f515
Rico Mitchell <ricomitch@icloud.com>
2020-04-08 22:50:07 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,8 +1,19 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
+
+  #this methods deines the code to execute when migration is run
+  #def up
+  #end
+  #this deines the code to execute when the migration is rolled back
+  #def down
+  #end
+  #primary way of writing migrations
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 
-  def down
-  end
 end
-      
